a34163d2102723243698d055935e6ba69b24126a
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-02-03 17:11:55 +0100

    Exercise 81: TestStrictBankAccount.java implmentation completed

--- 81-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 81-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -5,6 +5,10 @@ import it.unibo.bank.api.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -12,7 +16,14 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 class TestStrictBankAccount {
 
-    // Create a new AccountHolder and a StrictBankAccount for it each time tests are executed.
+    private static final int AMOUNT = 100;
+    private static final int NEG_AMOUNT = -40;
+    public static final double MANAGEMENT_FEE = 5;
+    public static final double TRANSACTION_FEE = 0.1;
+    private static final int ACCEPTABLE_MESSAGE_LENGTH = 10;
+
+    // Create a new AccountHolder and a StrictBankAccount for it each time tests are
+    // executed.
     private AccountHolder mRossi;
     private BankAccount bankAccount;
 
@@ -21,7 +32,8 @@ class TestStrictBankAccount {
      */
     @BeforeEach
     public void setUp() {
-        fail("To be implemented");
+        this.mRossi = new AccountHolder("Mario", "Rossi", 1);
+        this.bankAccount = new StrictBankAccount(mRossi, 0.0);
     }
 
     /**
@@ -29,15 +41,26 @@ class TestStrictBankAccount {
      */
     @Test
     public void testInitialization() {
-        fail("To be implemented");
+        assertEquals(0.0, bankAccount.getBalance());
+        assertEquals(0, bankAccount.getTransactionsCount());
+        assertEquals(mRossi, bankAccount.getAccountHolder());
     }
 
     /**
-     * Perform a deposit of 100€, compute the management fees, and check that the balance is correctly reduced.
+     * Perform a deposit of 100€, compute the management fees, and check that the
+     * balance is correctly reduced.
      */
     @Test
     public void testManagementFees() {
-        fail("To be implemented");
+        assertEquals(0.0, this.bankAccount.getBalance());
+        assertEquals(0, this.bankAccount.getTransactionsCount());
+        this.bankAccount.deposit(this.mRossi.getUserID(), AMOUNT);
+        assertEquals(100, this.bankAccount.getBalance());
+        assertEquals(1, this.bankAccount.getTransactionsCount());
+        double fees = MANAGEMENT_FEE + this.bankAccount.getTransactionsCount() * TRANSACTION_FEE;
+        this.bankAccount.chargeManagementFees(this.mRossi.getUserID());
+        assertEquals(AMOUNT - fees, this.bankAccount.getBalance());
+        assertEquals(0, this.bankAccount.getTransactionsCount());
     }
 
     /**
@@ -45,7 +68,15 @@ class TestStrictBankAccount {
      */
     @Test
     public void testNegativeWithdraw() {
-        fail("To be implemented");
+        try {
+            this.bankAccount.withdraw(this.mRossi.getUserID(), NEG_AMOUNT);
+            fail("Withdrawing a negative amount should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals(0, bankAccount.getBalance());
+            assertNotNull(e.getMessage()); // Non-null message
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH); // A message with a decent length
+        }
     }
 
     /**
@@ -53,6 +84,17 @@ class TestStrictBankAccount {
      */
     @Test
     public void testWithdrawingTooMuch() {
-        fail("To be implemented");
+        this.bankAccount.deposit(this.mRossi.getUserID(), AMOUNT);
+        assertEquals(100, this.bankAccount.getBalance());
+        assertEquals(1, this.bankAccount.getTransactionsCount());
+        try {
+            this.bankAccount.withdraw(this.mRossi.getUserID(), 2 * AMOUNT);
+            fail("Withdrawing more money than it is in the account should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals(100, bankAccount.getBalance());
+            assertNotNull(e.getMessage()); // Non-null message
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH); // A message with a decent length
+        }
     }
 }
