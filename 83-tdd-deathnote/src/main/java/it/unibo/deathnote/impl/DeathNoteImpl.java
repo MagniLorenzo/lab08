e5f8c0b1812406b8037b5132172e608c68cf2f20
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-02-05 20:03:10 +0100

    Exercise 83: first implementation of DeathNoteImpl.java

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -1,5 +1,41 @@
 package it.unibo.deathnote.impl;
 
-public class DeathNoteImpl {
-    
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote {
+
+    @Override
+    public String getRule(int ruleNumber) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void writeName(String name) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        throw new RuntimeException();
+    }
 }
