d2c2d3347c426770e8155ab41e1e14214b0685f8
Lorenzo Magni <lorenzo.magni6@studio.unibo.it>
2024-02-06 16:13:58 +0100

    Exercise 83: getRule test created

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,10 +1,34 @@
 package it.unibo.deathnote;
 
 import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class for the {@link DeathNote} class.
  */
 class TestDeathNote {
+
+    private DeathNote dNote = new DeathNoteImpl();
+
+    @Test
+    public void testGetRule() {
+        try {
+            dNote.getRule(0);
+            fail("Getting the rule in position 0 should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+        }
+        try {
+            dNote.getRule(-1);
+            fail("Getting the rule in a negative position should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+        }
+    }
 
 }
